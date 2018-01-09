686252e2342299e2d6538b3566b19fc79e6a390e
Santhosh Kumar Sellavel <skrece22@gmail.com>
2018-01-10 04:35:41 +0530

    add post todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model("User", {
+  email: {
+    type: String,
+    minlength: 1,
+    required: true,
+    trim: true
+  }
+});
+
+module.exports = { User };
