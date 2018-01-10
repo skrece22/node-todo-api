0897a9dc733738dc5d8f02728972aea675c4fe78
Santhosh Kumar Sellavel <skrece22@gmail.com>
2018-01-10 06:44:02 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -9,28 +10,50 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
-    
-    todo.save().then((doc) => {
-        res.status(200).send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
+app.post("/todos", (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(
+    (doc) => {
+      res.status(200).send(doc);
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
 });
 
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({ todos});
-    }, (e) => {
-            res.status(400).send(e);
-    });
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    (todos) => {
+      res.send({ todos });
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.send({todo});
+    }).catch((e) => res.status(400).send());
+
 });
 
 app.listen("3000", () => {
   console.log("Started app on port 3000.");
 });
 
-module.exports = {app}
+module.exports = { app };
